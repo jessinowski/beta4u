50f7702806ee0d8e5b8a397d79a29d917a862639
jessi <jessi.nowski@hotmail.de>
2024-03-20 16:47:10 +0100

    fixed unit tests

--- backend/src/test/java/com/github/backend/service/BoulderServiceTest.java
+++ backend/src/test/java/com/github/backend/service/BoulderServiceTest.java
@@ -1,6 +1,8 @@
 package com.github.backend.service;
 
 import com.github.backend.models.Boulder;
+import com.github.backend.models.Comment;
+import com.github.backend.models.User;
 import com.github.backend.models.enums.*;
 import com.github.backend.repo.BoulderRepo;
 import org.junit.jupiter.api.Test;
@@ -31,14 +33,30 @@ class BoulderServiceTest {
     @Test
     void getAllBoulders_returnListOfOne_whenCalledWithOneBoulder(){
         //GIVEN
-        List<Boulder> expected =List.of(new Boulder("1",
+        List<Boulder> expected =List.of(new Boulder(
+                "1",
                 "image",
                 "video",
                 Level.EIGHT,
                 Sector.FIVE,
                 Gym.UA_HH_OST,
                 null,
-                List.of(),
+                List.of(new Comment(
+                        "11",
+                        "nice slab!",
+                        new User(
+                                "22",
+                                "jurassica",
+                                "Jessica",
+                                "image",
+                                Gym.UA_HH_OST,
+                                List.of(Hold.CRIMP),
+                                List.of(Style.MANTLE),
+                                List.of(),
+                                List.of(),
+                                List.of(),
+                                List.of()),
+                        null)),
                 List.of(),
                 Routesetter.ALEX,
                 Color.BLUE,
