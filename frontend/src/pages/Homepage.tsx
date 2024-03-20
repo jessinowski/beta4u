dfc840abc4f99473f0c8e308623d95cb2e92b3dc
jessi <jessi.nowski@hotmail.de>
2024-03-20 23:00:48 +0100

    add navigationBar and homepage

---
+++ frontend/src/pages/Homepage.tsx
@@ -0,0 +1,22 @@
+import {useEffect, useState} from "react";
+import {Boulder} from "../types/Boulder.ts";
+import axios from "axios";
+
+export default function Homepage(){
+    const [boulders, setBoulders] = useState<Boulder[]>([]);
+
+    useEffect(fetchData, []);
+
+    if (!boulders) {
+        return "Loading..."
+    }
+
+    function fetchData() {
+        axios.get("/api/boulders")
+            .then(response => setBoulders(response.data))
+            .catch(error => {
+                console.error("Error fetching boulder", error)
+            })
+    }
+    return(<div><>{boulders.map(boulder => boulder.id)}</></div>)
+}
