a76cbf7772d5e631be7989ba0b90863b9d7ad45e
jessi <jessi.nowski@hotmail.de>
2024-03-20 15:58:40 +0100

    updated rating model

--- backend/src/main/java/com/github/backend/models/Rating.java
+++ backend/src/main/java/com/github/backend/models/Rating.java
@@ -3,12 +3,10 @@ package com.github.backend.models;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.With;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 
 @Data
-@With
 @NoArgsConstructor
 @AllArgsConstructor
 public class Rating {
