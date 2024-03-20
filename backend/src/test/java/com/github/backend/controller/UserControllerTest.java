a1fdfee22866a6ed4dbb6908cda464bc2a66b6a5
jessi <jessi.nowski@hotmail.de>
2024-03-20 17:25:55 +0100

    add User-GET-Endpoint

---
+++ backend/src/test/java/com/github/backend/controller/UserControllerTest.java
@@ -0,0 +1,74 @@
+package com.github.backend.controller;
+import com.github.backend.models.User;
+import com.github.backend.models.enums.*;
+import com.github.backend.repo.UserRepo;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.List;
+@SpringBootTest
+@AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+class UserControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private UserRepo repo;
+
+    @Test
+    void getAllUsers_returnEmptyList_whenCalledInitially() throws Exception{
+        //GIVEN
+        //WHEN & THEN
+        mvc.perform(MockMvcRequestBuilders.get("/api/users"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json("[]"))
+                .andReturn();
+    }
+
+    @Test
+    void getAllUsers_returnOneBoulder_whenCalled() throws Exception {
+        //GIVEN
+        User user= new User(
+                "22",
+                "jurassica",
+                "Jessica",
+                "image",
+                Gym.UA_HH_OST,
+                List.of(Hold.CRIMP),
+                List.of(Style.MANTLE),
+                List.of(),
+                List.of(),
+                List.of(),
+                List.of());
+        repo.save(user);
+        //WHEN & THEN
+        mvc.perform(MockMvcRequestBuilders.get("/api/users"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json("""
+                        [
+                        {
+                            "id": "22",
+                            "username": "jurassica",
+                            "fullName": "Jessica",
+                            "imagePath": "image",
+                            "homeGym": "UA_HH_OST",
+                            "favoriteHolds": ["CRIMP"],
+                            "favoriteStyles": ["MANTLE"],
+                            "myFavorites": [],
+                            "myTops": [],
+                            "myFlashes": [],
+                            "myProjects": []
+                        }
+                        ]
+                """))
+                .andReturn();
+    }
+}
