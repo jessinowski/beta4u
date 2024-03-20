a5e89b2adefb0ff6c27252b5d3f90419493f704b
jessi <jessi.nowski@hotmail.de>
2024-03-20 13:44:50 +0100

    updated model comment

---
+++ backend/src/main/java/com/github/backend/models/Comment.java
@@ -0,0 +1,24 @@
+package com.github.backend.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.With;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+@Data
+@With
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "comments")
+public class Comment {
+    @Id
+    private String id;
+    private String content;
+    @DBRef
+    private User user;
+    private LocalDateTime date;
+}
