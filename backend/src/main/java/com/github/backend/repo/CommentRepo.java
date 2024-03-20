c99444c6a57ae674861e081766d7e14872e96d08
jessi <jessi.nowski@hotmail.de>
2024-03-20 20:15:54 +0100

    rollback

--- backend/src/main/java/com/github/backend/repo/CommentRepo.java
+++
@@ -1,9 +0,0 @@
-package com.github.backend.repo;
-
-import com.github.backend.models.Comment;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CommentRepo extends MongoRepository<Comment,String> {
-}
