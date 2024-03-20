089b98a9c1d51e518a4132e8c01a7737640d31fc
jessi <jessi.nowski@hotmail.de>
2024-03-20 18:05:03 +0100

    updated model as records

--- backend/src/main/java/com/github/backend/models/Boulder.java
+++ backend/src/main/java/com/github/backend/models/Boulder.java
@@ -1,37 +1,30 @@
 package com.github.backend.models;
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.github.backend.models.enums.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.With;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
-
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+@With
 @Document(collection = "boulders")
-public class Boulder {
+public record Boulder (
     @Id
-    private String id;
-    private String imagePath;
-    private String videoPath;
-    private Level level;
-    private Sector sector;
-    private Gym gym;
-    private LocalDateTime date;
-    @DBRef(lazy = true)
+    String id,
+    String imagePath,
+    String videoPath,
+    Level level,
+    Sector sector,
+    Gym gym,
+    LocalDateTime date,
+    @DBRef
     @JsonIgnore
-    private List<Comment> comments = new ArrayList<>();
-    private List<Rating> ratings = new ArrayList<>();
-    private Routesetter routesetter;
-    private Color color;
-    private List<Hold> holds = new ArrayList<>();
-    private List<Style> styles = new ArrayList<>();
-}
+    List<Comment> comments,
+    List<Rating> ratings,
+    Routesetter routesetter,
+    Color color,
+    List<Hold> holds,
+    List<Style> styles
+){}
--- backend/src/main/java/com/github/backend/models/Comment.java
+++ backend/src/main/java/com/github/backend/models/Comment.java
@@ -1,22 +1,17 @@
 package com.github.backend.models;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.With;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+@With
 @Document(collection = "comments")
-public class Comment {
+public record Comment (
     @Id
-    private String id;
-    private String content;
+    String id,
+    String content,
     @DBRef
-    private User user;
-    private LocalDateTime date;
-}
+    User user,
+    LocalDateTime date
+){}
