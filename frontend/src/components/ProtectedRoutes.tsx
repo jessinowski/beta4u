ad2cf8e18cab21d94ecd9b2940dc32dd0bcd8e95
jessi <jessi.nowski@hotmail.de>
2024-03-22 18:45:46 +0100

    marked ProtectedRoutesProps as readonly

--- frontend/src/components/ProtectedRoutes.tsx
+++ frontend/src/components/ProtectedRoutes.tsx
@@ -3,7 +3,7 @@ import {Navigate, Outlet} from "react-router-dom";
 type ProtectedRoutesProps={
     user: string | null | undefined;
 }
-export default function ProtectedRoutes(props: ProtectedRoutesProps) {
+export default function ProtectedRoutes(props: Readonly<ProtectedRoutesProps>) {
 
     if (props.user === undefined) {
         return <div>Loading...</div>
