df58513bd5d76ed3ffe6b47201fff439875b2b9f
jessi <jessi.nowski@hotmail.de>
2024-03-25 09:55:45 +0100

    add selectFields in SignUpPage

--- frontend/src/pages/SignUpPage.tsx
+++ frontend/src/pages/SignUpPage.tsx
@@ -1,8 +1,9 @@
 import {ChangeEvent, FormEvent, useEffect, useState} from "react";
 import axios from "axios";
 import {User} from "../types/User.ts";
-import {Avatar} from "@mui/material";
+import {Avatar, FormControl, InputLabel, MenuItem, OutlinedInput, Select, SelectChangeEvent} from "@mui/material";
 import {useNavigate} from "react-router-dom";
+import {Gym, Hold, Style} from "../types/enums/gym.ts";
 
 type SignUpPageProps={
     fetchUser: ()=>void;
@@ -14,6 +15,9 @@ export default function SignUpPage(props: Readonly<SignUpPageProps>){
     const [styles, setStyles]=useState<string[]>([]);
     const [avatarURL, setAvatarURl]=useState<string>("");
     const navigate =useNavigate();
+    const optionalGyms=Object.values(Gym);
+    const optionalHolds=Object.values(Hold);
+    const optionalStyles=Object.values(Style);
 
     useEffect(showAvatar, []);
     function showAvatar(){
@@ -44,6 +48,34 @@ export default function SignUpPage(props: Readonly<SignUpPageProps>){
         }));
     }
 
+    function changeHomeGym(event: SelectChangeEvent<typeof homeGym>){
+        const value = event.target.value;
+        setHomeGym(value);
+        setFormData((prevData) => ({
+            ...prevData,
+            homeGym: homeGym,
+        }));
+    }
+    function changeFavoriteHolds(event: SelectChangeEvent<typeof holds>) {
+        const value = event.target.value;
+        const updatedHolds = typeof value === 'string' ? value.split(',') : value;
+        setHolds(updatedHolds);
+        setFormData((prevData) => ({
+            ...prevData,
+            favoriteHolds: updatedHolds,
+        }));
+    }
+
+    function changeFavoriteStyles(event: SelectChangeEvent<typeof styles>) {
+        const value = event.target.value;
+        const updatedStyles = typeof value === 'string' ? value.split(',') : value;
+        setStyles(updatedStyles);
+        setFormData((prevData) => ({
+            ...prevData,
+            favoriteStyes: updatedStyles,
+        }));
+    }
+
 
     return(
         <div>
@@ -52,15 +84,71 @@ export default function SignUpPage(props: Readonly<SignUpPageProps>){
             <Avatar alt="Remy Sharp" src={avatarURL} />
             <form onSubmit={handleOnSubmit}>
                 <label> Username:
-                    <input type={"text"} id={"username"} name={"username"} value={formData.username} onChange={handleChangeUsername} required/>
+                    <input type={"text"} id={"username"} name={"username"} value={formData.username}
+                           onChange={handleChangeUsername} required/>
                 </label>
+                <br/>
                 <label> Full name:
-                    <input type={"text"} id={"fullName"} name={"fullName"} value={formData.fullName} onChange={handleChangeFullName}/>
+                    <input type={"text"} id={"fullName"} name={"fullName"} value={formData.fullName}
+                           onChange={handleChangeFullName}/>
                 </label>
-
-                <p>homeGym, selectField</p>
-                <p>holds, selectField</p>
-                <p>styles, selectField</p>
+                <br/>
+                <FormControl sx={{m: 1, width: 300}}>
+                    <InputLabel>Home gym</InputLabel>
+                    <Select
+                        value={homeGym}
+                        onChange={changeHomeGym}
+                        input={<OutlinedInput label="Home gym"/>}
+                    >
+                        {optionalGyms.map(gym =>
+                            <MenuItem
+                                key={gym}
+                                value={gym}
+                            >
+                                {gym}
+                            </MenuItem>
+                        )}
+                    </Select>
+                </FormControl>
+                <br/>
+                <FormControl sx={{m: 1, width: 300}}>
+                    <InputLabel>Favorite holds</InputLabel>
+                    <Select
+                        multiple
+                        value={holds}
+                        onChange={changeFavoriteHolds}
+                        input={<OutlinedInput label="Favorite holds"/>}
+                    >
+                        {optionalHolds.map(hold =>
+                            <MenuItem
+                                key={hold}
+                                value={hold}
+                            >
+                                {hold}
+                            </MenuItem>
+                        )}
+                    </Select>
+                </FormControl>
+                <br/>
+                <FormControl sx={{m: 1, width: 300}}>
+                    <InputLabel>Favorite styles</InputLabel>
+                    <Select
+                        multiple
+                        value={styles}
+                        onChange={changeFavoriteStyles}
+                        input={<OutlinedInput label="Favorite styles"/>}
+                    >
+                        {optionalStyles.map(style =>
+                            <MenuItem
+                                key={style}
+                                value={style}
+                            >
+                                {style}
+                            </MenuItem>
+                        )}
+                    </Select>
+                </FormControl>
+                <br/>
                 <button type={"submit"}>Sign up</button>
             </form>
         </div>
---
+++ frontend/src/types/enums/gym.ts
@@ -0,0 +1,29 @@
+export enum Gym{
+    UA_HH_WEST="urban apes Hamburg West",
+    UA_HH_OST="urban apes Hamburg Ost",
+    UA_NDS="urban apes Norderstedt",
+    UA_ST_PAULI="urban apes St. Pauli"
+}
+
+export enum Hold{
+    CRIMP="crimp",
+    JUG="jug",
+    PINCH="pinch",
+    POCKET="pocket",
+    SLOPER="sloper",
+    VOLUME="volume",
+    MACRO="macro",
+    DUAL_TEX="dual tex"
+}
+
+export enum Style{
+    DYNO="dyno",
+    SLAB="slab",
+    ROOF="roof",
+    NEGATIVE="negative",
+    MANTLE="mantle",
+    RUN_UP="run up",
+    STATIC="static",
+    BAT_HANG="bat hang",
+    CORNER="corner"
+}
