5fe15eecbd953843e60d7078a5c90914d27d2e6f
jessi <jessi.nowski@hotmail.de>
2024-03-28 22:06:47 +0100

    add button navigation

--- frontend/src/pages/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage.tsx
@@ -1,19 +1,58 @@
-import {Outlet, useNavigate} from "react-router-dom";
-import {Divider, IconButton} from "@mui/material";
+import {useNavigate, useParams} from "react-router-dom";
+import {AppBar, IconButton, Tab, Tabs} from "@mui/material";
 import ManageAccountsIcon from '@mui/icons-material/ManageAccounts';
 import MapIcon from '@mui/icons-material/Map';
-import {User} from "../types/User.ts";
 import "./ProfilePage.css";
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
 import ElectricBoltIcon from '@mui/icons-material/ElectricBolt';
 import SquareFootIcon from '@mui/icons-material/SquareFoot';
+import {useEffect, useState} from "react";
+import MyFavorites from "./MyFavorites.tsx";
+import MyFlashes from "./MyFlashes.tsx";
+import MyTops from "./MyTops.tsx";
+import MyProjects from "./MyProjects.tsx";
+import MyLocations from "./MyLocations.tsx";
 
-type ProfilePageProps={
-    user: User;
-}
-export default function ProfilePage(props: Readonly <ProfilePageProps>){
-    const navigate=useNavigate();
+
+export default function ProfilePage() {
+    const navigate = useNavigate();
+    const {tabName} =useParams();
+    const [value, setValue] = useState<string>(tabName || "projects");
+
+    useEffect(() => {
+        if (tabName) setValue(tabName);
+    }, [tabName]);
+
+    const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+        setValue(newValue);
+        navigate("/profile/"+newValue);
+    };
+
+    let activeComponent;
+    switch(value){
+        case "favorites": {
+            activeComponent = <MyFavorites/>;
+            break;
+        }
+        case "flashes": {
+            activeComponent = <MyFlashes/>;
+            break;
+        }
+        case "tops": {
+            activeComponent = <MyTops/>;
+            break;
+        }
+        case "projects": {
+            activeComponent = <MyProjects/>;
+            break;
+        }
+        case "locations": {
+            activeComponent = <MyLocations />;
+            break;
+        }
+    }
+
 
     return (
         <div>
@@ -31,16 +70,24 @@ export default function ProfilePage(props: Readonly <ProfilePageProps>){
                     <IconButton onClick={() => navigate("/profile/locations")}><MapIcon/></IconButton>
                 </div>
             </div>
-            <Divider />
             <div className={"selectListBar"}>
-                <FavoriteBorderIcon/>
-                <ElectricBoltIcon/>
-                <CheckCircleOutlineIcon/>
-                <SquareFootIcon/>
+                <AppBar position="static" color="default">
+                    <Tabs
+                        value={value}
+                        onChange={handleChange}
+                        indicatorColor="primary"
+                        textColor="primary"
+                        variant="fullWidth"
+                        aria-label="action tabs example"
+                    >
+                        <Tab icon={<FavoriteBorderIcon/>} label="Favorites" value={"favorites"}/>
+                        <Tab icon={<ElectricBoltIcon/>} label="Flashes" value={"flashes"}/>
+                        <Tab icon={<CheckCircleOutlineIcon/>} label="Tops" value={"tops"}/>
+                        <Tab icon={<SquareFootIcon/>} label="Projects" value={"projects"}/>
+                    </Tabs>
+                </AppBar>
             </div>
-            <Divider />
-
-            <Outlet/>
+            {activeComponent}
         </div>
     )
 }
