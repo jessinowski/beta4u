6dadf3bb7bbba75ceb85f9894184d8e8f03d5e44
jessi <jessi.nowski@hotmail.de>
2024-04-02 12:54:28 +0200

    updated AddToMyLists component

--- frontend/src/components/AddToMyLists.tsx
+++ frontend/src/components/AddToMyLists.tsx
@@ -1,7 +1,8 @@
-import {IconButton, Menu, MenuItem} from "@mui/material";
+import {IconButton, Menu, MenuItem, NativeSelect} from "@mui/material";
 import {PostAdd} from "@mui/icons-material";
-import {useState} from "react"
+import {FormEventHandler, MouseEventHandler, useState} from "react"
 import {Boulder} from "../types/Boulder.ts";
+import axios from "axios";
 
 type AddToMyListProps={
     boulder: Boulder;
@@ -9,6 +10,9 @@ type AddToMyListProps={
 export default function AddToMyList(props: Readonly<AddToMyListProps>){
     const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
     const open = Boolean(anchorEl);
+    const [isFlash, setIsFlash]=useState<boolean>(false);
+    const [isTop, setIsTop]=useState<boolean>(false);
+    const [isProject, setIsProject]=useState<boolean>(false);
     const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
         setAnchorEl(event.currentTarget);
     };
@@ -17,7 +21,30 @@ export default function AddToMyList(props: Readonly<AddToMyListProps>){
         setAnchorEl(null);
     };
 
-
+    function getBoulderLists(value: string){
+        axios.get<Boulder[]>("/api/user/" + value)
+            .then(response => {
+                const addedBoulder = response.data.find(b => b.id === props.boulder.id);
+                switch(value){
+                    case "flashes":{
+                        setIsFlash(addedBoulder !== undefined);
+                        break;
+                    }
+                    case "tops":{
+                        setIsTop(addedBoulder !== undefined);
+                        break;
+                    }
+                    case "projects":{
+                        setIsProject(addedBoulder !== undefined);
+                        break;
+                    }
+                }
+            })
+    }
+    function handleChange(value: string){
+        return axios.put("/api/user/" + value + "/" + props.boulder.id)
+            .then(getBoulderLists(value));
+    }
 
     return(
         <div>
@@ -25,9 +52,11 @@ export default function AddToMyList(props: Readonly<AddToMyListProps>){
                 <PostAdd />
             </IconButton>
             <Menu anchorEl={anchorEl} open={open} onClose={handleClose}>
-                    <MenuItem onClick={handleClose}>Flash</MenuItem>
-                    <MenuItem onClick={handleClose}>Top</MenuItem>
-                    <MenuItem onClick={handleClose}>Project</MenuItem>
+                <NativeSelect>
+                    <option value={"flashes"} >Flash</option>
+                    <option value={"tops"}>Top</option>
+                    <option value={"projects"}>Project</option>
+                </NativeSelect>
             </Menu>
         </div>
     )
